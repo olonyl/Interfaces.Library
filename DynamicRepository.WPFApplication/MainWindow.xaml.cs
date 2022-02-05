2e132604fbcc1f1702a28123c7465d6dd9e75601
olonyl <olony.rocha@contollo.net>
2022-02-05 10:50:59 -0600

    Implemented Dynamic Repository

---
+++ DynamicRepository.WPFApplication/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using People.Repository.Itnerface;
+using System.Windows;
+
+namespace DynamicRepository.WPFApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DynamicReposositoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+            IPersonRepository repository = RepositoryFactory.GetRepository();
+            var peopple = repository.GetPeople();
+            PersonListBox.ItemsSource = peopple;
+            ShowRepositoryType(repository);
+
+        }
+
+        private void ClearListBox()
+        {
+            this.PersonListBox.ItemsSource = null;
+        }
+
+
+        private void ShowRepositoryType(IPersonRepository repository)
+        {
+            MessageBox.Show(string.Format("Repositry Type:\n{0}",
+                repository.GetType().ToString()));
+        }
+    }
+}
---
+++ DynamicRepository.WPFApplication/RepositoryFactory.cs
@@ -0,0 +1,25 @@
+﻿using People.Repository.Itnerface;
+using System;
+using System.Configuration;
+
+namespace DynamicRepository.WPFApplication
+{
+    public static class RepositoryFactory
+    {
+        public static IPersonRepository GetRepository()
+        {
+            string typeName = ConfigurationManager.AppSettings["RepositoryType"];
+            Type repoType = Type.GetType(typeName);
+            object repoInstance = Activator.CreateInstance(repoType);
+            IPersonRepository repo = repoInstance as IPersonRepository;
+            return repo;
+
+        }
+    }
+    public enum RepositoryType
+    {
+        SQL,
+        CSV,
+        Service
+    }
+}
